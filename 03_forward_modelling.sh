b272e2021fe8399ef992c794848635a4da99bfd0
SzuYing <33931460+lucine0629@users.noreply.github.com>
2025-07-21 17:52:04 +0100

    Update and rename p01_plot_vp.sh to 03_forward_modelling.sh

--- 03_forward_modelling.sh
+++ 03_forward_modelling.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-# make_fig.sh
+## Script: 03_forward_modelling.sh
+## Purpose: Run forward modelling for ray tracing using the inverted velocity model and plot the velocity model
+## Author: S.Y. Lai
 
+
+##############################
+## DEFINE plotting variables 
+##############################
 ps=B_"$1"_vp.ps
 ps1=B_"$1"vpnoray.ps
-
-
-## plotting variables 
 X=8.78 
 Z=5
 cpt=stmodel2ori.cpt
 bathy=p13_bathyf.txt ##50m bathymetry
 
-
-## tomo2d variables
+####################################
+## DEFINE FORWARD tomo2d variables
+####################################
 N=4/4/4/8/1e-2/1e-3
 ray=ray"$1".dat
 src=src.dat
@@ -25,18 +29,23 @@ grd=n"$1"_model.grd
 rm $ray ## clear the existing raypath files
 
 
-
+####################################################
+## Forward ray tracing and make the velocity grid
+####################################################
 tt_forward -M$smesh -Itmp.v0 -G$geom -R$ray -S$src -T$tt -N$N > tt_"$1".dat
 ./togrid.sh tmp.v0 0 $X 2.5 6 0.05 0.05 $grd
 
 
 
+
+
+
+##############################
+#### PLOT THE FINAL VP MODEL
+##############################
 gmt gmtset ANNOT_FONT_SIZE_PRIMARY=16
 gmt gmtset LABEL_FONT_SIZE=16
 gmt gmtset ANNOT_FONT_SIZE_SECONDARY=16
-
-
-
 
 gmt psbasemap -JX17/-9 -R0/$X/2.7/4.8 -Ba1:"Profile distance (km)":/a0.4f0.1:"Depth (km)":WeSn -Y5 -X2 -K > $ps
 gmt grdimage -C$cpt -J -R $grd -O -K >> $ps
@@ -55,8 +64,9 @@ gmt psxy $src -JX -R -Sc0.3 -G255/48/48 -W0.9 -O -K >> $ps1
 gmt psscale -C$cpt -Dx0.6/11+w7/0.2+h -Ba1f0.5 -B+l"Velocity (km/s)" -O >> $ps1
 
 
-
-
+#######################
+## CONVERT TO JPEG
+#######################
 gmt psconvert $ps $ps1 -A -Tj
 rm gmt.history
 
